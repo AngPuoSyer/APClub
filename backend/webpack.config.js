e6b124845155d5c55f3583f8b18ce331c8885598
Puo Syer Ang <angps.official@gmail.com>
2022-10-21 15:48:05 +0800

    chore(config): config update

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     alias: {
       '@core': path.resolve(__dirname, 'apps/backend/'),
       '@generated': path.resolve(__dirname, 'prisma/@generated'),
+      '@app/common': path.resolve(__dirname, 'libs/common/src')
     },
     extensions: ['', '.ts', '.tsx', '.js', '.jsx'],
   },
