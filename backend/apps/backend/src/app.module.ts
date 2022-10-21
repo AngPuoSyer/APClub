8880e18be1039ebdda4a515c981538c500478fde
Puo Syer Ang <angps.official@gmail.com>
2022-10-21 15:46:15 +0800

    chore(backend): (app) config to app module

--- backend/apps/backend/src/app.module.ts
+++ backend/apps/backend/src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppService } from './app.service';
-import config from './common/config/config';
+import config from '@app/common/config/config';
 import { PrismaModule } from 'nestjs-prisma';
 import { GraphQLModule } from '@nestjs/graphql';
-import { GraphqlConfig } from './common/config/config.interface';
+import { GraphqlConfig } from '../../../libs/common/src/config/config.interface';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ClubAdminModule } from './modules/club-admin/club-admin.module';
 import { SuperAdminModule } from './modules/super-admin/super-admin.module';
 import { ClubModule } from './modules/club/club.module';
-import { ClubRequestModule } from './modules/club-request/club-request.module';
+import { ClubInfoChangeRequestModule } from './modules/club-request/club-info-change-request.module';
+import { RmqModule } from '@app/common/rmq/rmq.module';
+import { APClubServices } from '@app/common/serivce/channels.enum';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    RmqModule.register(APClubServices.SCHEDULER),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       useFactory: async (configService: ConfigService) => {
@@ -54,16 +56,7 @@ import { ClubRequestModule } from './modules/club-request/club-request.module';
     ClubAdminModule,
     SuperAdminModule,
     ClubModule,
-    ClubRequestModule,
+    ClubInfoChangeRequestModule,
   ],
-  providers: [AppService],
 })
 export class AppModule {}
-
-// TODO: Migrate Individually
-// TODO: Role Guard (Refer to lavax)
-// TODO: super admin create club
-// TODO: admin make change request // task queue
-// TODO: super admin approve request
-// TODO: pagination
-// TODO: remmove password field
